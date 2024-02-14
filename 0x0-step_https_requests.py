aa0690278319a2b7d19e6c93fa8ba429727ba716
georgesmbock <georgesmbo89@gmail.com>
2024-02-14 17:28:50 +0100

    https request on url

--- 0x0-step_https_requests.py
+++ 0x0-step_https_requests.py
@@ -10,3 +10,10 @@ url = "https://codeavecjonathan.com/scraping/recette"
 import requests as rq
 
 answer = rq.get(url)
+
+if answer.status_code == 200:
+    
+    print("Great Work")
+    
+else :
+    print("ERROR")
